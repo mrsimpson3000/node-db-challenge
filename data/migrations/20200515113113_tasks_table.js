ee1287e2691a7dc0104641bf131bd4951f0ece17
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-15 11:35:32 -0400

    Migrations written and implemented. I hope I'm doing this right.

---
+++ data/migrations/20200515113113_tasks_table.js
@@ -0,0 +1,19 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("tasks", (tbl) => {
+    tbl.increments();
+    tbl.text("task_description", 255).notNullable();
+    tbl.text("task_notes", 255);
+    tbl.integer("task_completed").unsigned().defaultTo(0);
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("projects.id")
+      .onUpdate("CASCADE")
+      .onDelete("RESTRICT");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("tasks");
+};
