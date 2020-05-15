1c868cc3c6b9ad52c657a583ca1089e899b876f7
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-15 13:30:16 -0400

    GET all projects enpoint is up. If only I knew how to build a readme.

---
+++ projects/project-router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const Projects = require("./project-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.find()
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+module.exports = router;
